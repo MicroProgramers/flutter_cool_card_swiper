7a875c543850e99a2f0d99002af02c2ece424511
MicroProgramer <microprogramer@yahoo.com>
2022-06-10 00:32:50 +0500

    updated

--- lib/flutter_cool_card_swiper.dart
+++ lib/flutter_cool_card_swiper.dart
@@ -1,7 +1,5 @@
 library flutter_cool_card_swiper;
+export 'package:flutter_cool_card_swiper/widgets/cool_swiper.dart';
+class CoolSwiper{
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
 }
